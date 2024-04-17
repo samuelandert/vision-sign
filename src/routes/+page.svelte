403658c6fbfc4c0c69191ba19e5e762a120160b4
Samuel Andert <samuel@andert.me>
2024-04-17 12:24:49 +0200

    fix register and sign in flow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { connectProvider, registerWithWebAuthn, authenticateWithWebAuthn } from '../lib/litSetup';
+	import { browser } from '$app/environment';
 
 	let isConnected = false;
 	let pkpPublicKey = '';
@@ -9,29 +10,37 @@
 		try {
 			await connectProvider();
 			isConnected = true;
+			// Attempt to retrieve an existing passkey from local storage
+			if (browser) {
+				pkpPublicKey = localStorage.getItem('pkpPublicKey') || '';
+			}
 		} catch (error) {
 			console.error('Failed to connect to provider:', error);
 			isConnected = false;
 		}
 	});
 
-	async function handleSignIn() {
-		// Check if a pkpPublicKey already exists
-		if (!pkpPublicKey) {
-			// No passkey exists, proceed with registration
-			try {
-				pkpPublicKey = await registerWithWebAuthn();
-				alert(`PKP Public Key: ${pkpPublicKey}`);
-				// After registration, automatically proceed with authentication
-				await authenticate();
-			} catch (error) {
-				console.error('Registration failed:', error);
-				return; // Exit the function if registration fails
+	async function handleRegister() {
+		try {
+			pkpPublicKey = await registerWithWebAuthn();
+			alert(`PKP Public Key: ${pkpPublicKey}`);
+			// Store the new passkey in local storage
+			if (browser) {
+				localStorage.setItem('pkpPublicKey', pkpPublicKey);
 			}
-		} else {
-			// Passkey exists, proceed with authentication
+			// Automatically authenticate after registration
 			await authenticate();
+		} catch (error) {
+			console.error('Registration failed:', error);
 		}
+	}
+
+	async function handleSignIn() {
+		if (!pkpPublicKey) {
+			alert('No passkey found. Please register first.');
+			return;
+		}
+		await authenticate();
 	}
 
 	async function authenticate() {
@@ -44,17 +53,22 @@
 	}
 </script>
 
-{#if isConnected}
-	<p>Lit Protocol is connected!</p>
-	<div class="p-4">
-		<button class="px-4 py-2 text-white rounded-lg bg-blue-950" on:click={handleSignIn}
-			>Sign In</button
-		>
-	</div>
+<div class="w-screen h-screen bg-orange-50">
+	{#if isConnected}
+		<div class="p-2 bg-yellow-400"><p>Lit Protocol is connected!</p></div>
+		<div class="p-4">
+			<button class="px-4 py-2 text-white rounded-lg bg-blue-950" on:click={handleRegister}
+				>Register</button
+			>
+			<button class="px-4 py-2 text-white bg-green-500 rounded-lg" on:click={handleSignIn}
+				>Sign In</button
+			>
+		</div>
 
-	{#if pkpPublicKey}
-		<p>PKP Public Key: {pkpPublicKey}</p>
+		{#if pkpPublicKey}
+			<p class="p-4">PKP Public Key: {pkpPublicKey}</p>
+		{/if}
+	{:else}
+		<div class="p-2 bg-orange-400"><p>Connecting to Lit Protocol...</p></div>
 	{/if}
-{:else}
-	<p>Connecting to Lit Protocol...</p>
-{/if}
+</div>
