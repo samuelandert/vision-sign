38dd54d5b8ca2ef13c7a0ec0e2323af76129cb94
Samuel Andert <samuel@andert.me>
2024-04-19 16:35:33 +0200

    updated SSL dev only vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,26 +4,32 @@ import fs from 'fs';
 import path from 'path';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
+// Check if the current environment is development
+const isDevelopment = process.env.NODE_ENV === 'development';
+
 export default defineConfig({
 	server: {
-		https: {
-			key: fs.readFileSync(path.resolve('localhost-key.pem'), 'utf-8'),
-			cert: fs.readFileSync(path.resolve('localhost.pem'), 'utf-8')
-		}
+		...(isDevelopment && {
+			https: {
+				key: fs.readFileSync(path.resolve('localhost-key.pem'), 'utf-8'),
+				cert: fs.readFileSync(path.resolve('localhost.pem'), 'utf-8')
+			}
+		})
 	},
-	plugins: [sveltekit(),
-	nodePolyfills({
-		include: ['path', 'stream', 'util'],
-		exclude: ['http'],
-		globals: {
-			Buffer: true,
-			global: true,
-			process: true,
-		},
-		overrides: {
-			fs: 'memfs',
-		},
-		protocolImports: true,
-	}),],
-
+	plugins: [
+		sveltekit(),
+		nodePolyfills({
+			include: ['path', 'stream', 'util'],
+			exclude: ['http'],
+			globals: {
+				Buffer: true,
+				global: true,
+				process: true,
+			},
+			overrides: {
+				fs: 'memfs',
+			},
+			protocolImports: true,
+		}),
+	],
 });
