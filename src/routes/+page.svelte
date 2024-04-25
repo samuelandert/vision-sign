a7e8c994112652c1222d7afd90755dc8c11e4860
Samuel Andert <samuel@andert.me>
2024-04-25 16:16:05 +0200

    some more tiny use upgrades

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import { meStore, pkpWalletStore } from '$lib/stores';
 	import { registerWithWebAuthn, authenticateWithWebAuthn } from '$lib/webAuthn';
 	import { sendTxWithPKPWallet } from '$lib/sendTxWithPKPWallet';
+	import Icon from '@iconify/svelte';
 
 	let isSignedIn = false;
 
@@ -44,42 +45,56 @@
 		localStorage.removeItem('lit-session-key');
 		isSignedIn = false;
 	}
+
+	function refreshPage() {
+		window.location.reload();
+	}
 </script>
 
 <div class="flex flex-col w-screen h-screen">
 	<div class="flex-grow h-full overflow-auto">
 		{#if isSignedIn}
-			<div class="p-4">
-				<Transactions />
+			<div class="flex justify-center p-4">
+				<div class="w-full max-w-6xl">
+					<Transactions />
+				</div>
 			</div>
 		{:else}
 			<div class="flex items-center justify-center flex-grow w-full h-full background-image">
-				<p class="px-4 text-2xl font-bold text-center text-white uppercase break-words sm:px-0">
+				<p
+					class="px-4 text-3xl font-bold text-center text-white uppercase break-words sm:text-6xl sm:px-0"
+				>
 					your true life starts now
 				</p>
 			</div>
 		{/if}
 	</div>
-	<div
-		class="fixed bottom-0 left-0 right-0 flex justify-center mx-auto shadow-md rounded-t-3xl bg-blue-950"
-	>
-		<div class="inline-flex items-center justify-start w-full p-3 px-5 space-x-4">
-			<img src="/logo.svg" alt="Logo" class="h-10 border-white rounded-full" />
-			{#if isSignedIn}
-				<button class="px-4 py-2 text-white rounded-full bg-cyan-600" on:click={handleLogout}>
-					Logout
+	<div class="fixed bottom-0 left-0 right-0 flex justify-center mx-auto shadow-md">
+		<div class="w-full max-w-6xl pb-1 bg-blue-950 rounded-t-3xl">
+			<div class="inline-flex items-center justify-start w-full p-3 px-5 space-x-4">
+				<img src="/logo.svg" alt="Logo" class="h-10 border-white rounded-full" />
+				<button class="text-white rounded-full bg-blue-950" on:click={refreshPage}>
+					<Icon icon="solar:refresh-circle-linear" class="w-10 h-10" />
 				</button>
-				<button class="px-4 py-2 bg-yellow-400 rounded-full text-blue-950" on:click={sendXDai}>
-					Donate 0.01$
-				</button>
-			{:else}
-				<button class="px-4 py-2 bg-yellow-400 rounded-full text-blue-950" on:click={handleSignIn}>
-					Sign In
-				</button>
-				<button class="px-4 py-2 text-white rounded-full bg-cyan-600" on:click={handleRegister}>
-					Register
-				</button>
-			{/if}
+				{#if isSignedIn}
+					<button class="text-white rounded-full bg-blue-950" on:click={handleLogout}>
+						<Icon icon="solar:logout-outline" class="w-10 h-10" />
+					</button>
+					<button class="px-4 py-2 bg-yellow-400 rounded-full text-blue-950" on:click={sendXDai}>
+						Donate 0.01$
+					</button>
+				{:else}
+					<button
+						class="px-4 py-2 bg-yellow-400 rounded-full text-blue-950"
+						on:click={handleSignIn}
+					>
+						Sign In
+					</button>
+					<button class="px-4 py-2 text-white rounded-full bg-cyan-600" on:click={handleRegister}>
+						Register
+					</button>
+				{/if}
+			</div>
 		</div>
 	</div>
 </div>
